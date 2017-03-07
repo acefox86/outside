0d4626274dc4cdfc5e4208f7d15e7637c9bcb79f
Owen Fernau <owenfernau@gmail.com>
2017-03-07 09:24:10 -0800

    file setup

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-function setup() {
+var y = 10;
 
+function setup() {
+	createCanvas(500,500);
+	stroke(200);
+	frameRate(20);
 }
 
 function draw() {
-	ellipse(100, 10, 80, 80);  
+	ellipse(10, 10, 10, y);
+	/*background(100);
+	y = y - 1;
+	if (y < 0); {
+		y = height;
+	}
+	line(0, y, width, y);*/
+	y++;
 }
