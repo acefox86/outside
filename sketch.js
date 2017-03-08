8909d2a541208ba059f7cec2796beb85f78b43e3
Owen Fernau <owenfernau@gmail.com>
2017-03-07 19:29:34 -0800

    closer

--- sketch.js
+++ sketch.js
@@ -7,28 +7,45 @@ function setup() {
 	//stroke(200);
 	fill(255, 204)
 	background(100);
-	frameRate(50);
+	frameRate(10);
 	//noStroke();
-	//sphere1 = new sphere(10, 10, 40, 40);
+	sphere1 = new Sphere(50, 50, 40, 40);
+	//sphere2 = new Sphere
 }
 
 function draw() {
 	
-	ellipse(x, y, 60, 60);
-	//sphere1 = new sphere(10, 10, 40, 40);
-	//sphere1.display();
-	x++;
-	y++;
+	//ellipse(x, y, 60, 60);
+	console.log("hello");
+	//sphere1 = new Sphere(10, 10, 40, 40);
+	sphere1.step();
+	sphere1.display();
+
 }
 
-function sphere(x, y, w, h) {
+//function Sphere
+
+
+function Sphere(x, y, w, h) {
 	this.xLoc = x;
 	this.yLoc = y;
 	this.sWidth = w;
 	this.sHeight = h;
+	
+	this.step = function() {
+		var stepX = random(10);
+		var stepY = random(10);
+		
+		x += stepX;
+		y += stepY;
+		
+		console.log(x);
+	}
 
 	this.display = function() {
 		fill(35, 4, 16)
-		ellipse(xLoc, yLoc, sWidth, sHeight);
+		ellipse(x, y, this.sWidth, this.sHeight);
 	}
 }
+
+
