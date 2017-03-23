3f88464f179fa4d5c9dc0b2aca7f7fe47ba24d00
Owen Fernau <owenfernau@gmail.com>
2017-03-23 07:35:20 -0700

    bouncing ball with custom objects

--- messingaround.js
+++ messingaround.js
@@ -1,21 +1,34 @@
 
+
+
+
 console.log("hi");
 
 var shape1 = new VectorObject(50, 30);
-var shape2 = new VectorObject(60, 70);
+var shape2 = new VectorObject(10, 5);
 
 function setup() {
-	createCanvas(400, 400);
-	frameRate(5);
+	createCanvas(500, 500);
+	frameRate(0);
 }
 
 function draw() {
-	
+	background(100, 200, 200);
+	fill(50, 60, 70);
 	ellipse(shape1.x, shape1.y, 50, 50);
-	ellipse(shape2.x, shape2.y, 50, 50);
-	var added = shape1.add(shape2);
-	console.log(added.x);
-	ellipse(added.x, added.y, 30, 30);
+	
+	//var added = shape1.add(shape2);
+	shape1 = shape1.add(shape2);
+	//console.log(added.x);
+	
+	ellipse(shape1.x, shape1.y, 30, 30);
+
+	if (shape1.x > width || shape1.x < 0) {
+		shape2.x = (shape2.x * -1);
+	}
+	if (shape1.y > height || shape1.y < 0) {
+		shape2.y = (shape2.y * -1);
+	}
 }
 
 function VectorObject (x,y) {
@@ -27,6 +40,7 @@ VectorObject.prototype.add = function (VecObj) {
 	
 	var addedX = this.x + VecObj.x;
 	var addedY = this.y + VecObj.y;
+
 
 	return new VectorObject(addedX, addedY);
 }
